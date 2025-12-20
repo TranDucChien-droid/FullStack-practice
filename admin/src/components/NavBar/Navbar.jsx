b68c7f94f55526492a5765f3336a03929087010b
Chien <chientd99@gmail.com>
2025-12-20 23:58:32 +0700

    Fix: Admin page logout Button

--- admin/src/components/NavBar/Navbar.jsx
+++ admin/src/components/NavBar/Navbar.jsx
@@ -1,11 +1,20 @@
 import css from './Navbar.module.css';
 import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+import Button from '../Button/Button';
 
 export default function Navbar() {
+	const navigate = useNavigate();
+
+	const onLogoutClick = () => {
+		localStorage.removeItem('access_token');
+		navigate('/login');
+	};
+
 	return (
 		<div className={css['container']}>
 			<img className={css['logo']} src={logo} />
-			<button className={css['btn']}>Logout</button>
+			<Button onClick={onLogoutClick}>Logout</Button>
 		</div>
 	);
 }
